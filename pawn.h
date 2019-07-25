4a4d4ed6ad80c2d6e25b31cb6e512f195d6ff7e9
btgent <btgent@hotmail.com>
2019-07-25 17:30:48 -0400

    Update pawn.h

--- pawn.h
+++ pawn.h
@@ -1,9 +1,16 @@
 #ifndef _PAWN_H_
 #define _PAWN_H_
+#include "piece.h"
+#include <vector>
+using std::vector;
 
 class Pawn: public Piece {
+  vector<Coord> moves;
+  vector<Coord> attack;
  public:
+  Pawn(Coord pos, Colour colour);
   bool possibleMove(Coord Pos);
+  bool possibleAttack(Coord Pos);
 
 };
 
