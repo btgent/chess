62a25e548e0d4835d783a43544cbff7590918b8d
btgent <btgent@hotmail.com>
2019-07-26 04:24:30 +0800

    Update piece.h

--- piece.h
+++ piece.h
@@ -1,16 +1,15 @@
 #ifndef _PIECE_H_
 #define _PIECE_H_
+#include "type.h"
+#incldue "coord.h"
 
 class Piece {
   Coord pos;
   Type type;
   Colour colour;
  public:
-  bool virtual possibleMove(Coord) = 0;
-  bool virtual possibleMove(int row, int col) = 0;
-  void virtual setPos(Coord );
-  
-
+  bool virtual possibleMove(Coord Pos) = 0;
+  void virtual setPos(Coord Pos) = 0;
 }
 
 #endif
