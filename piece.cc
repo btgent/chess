efca3ea4a266c706390711c3ea25d937ec737cdd
btgent <btgent@hotmail.com>
2019-07-25 17:19:46 -0400

    Update piece.cc

--- piece.cc
+++ piece.cc
@@ -2,6 +2,8 @@
 
 Piece::Piece(Coord pos, Type type, Colour colour): pos{pos}, type{type}, colour{colour} {}
 
+Piece::~Piece() {}
+
 Piece::setPos(Coord Pos) {
   pos->row = Pos->row;
   pos->col = Pos->col;
