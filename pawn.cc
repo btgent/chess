c15edfc7796b51433d8b71441055de0d549d3f0c
btgent <btgent@hotmail.com>
2019-07-25 21:01:12 -0400

    Update pawn.cc

--- pawn.cc
+++ pawn.cc
@@ -2,9 +2,37 @@
 
 Pawn::Pawn(Coord Pos, Colour colour): pos{Pos}, colour{colour} {
   type = Pawn;
-  Coord move1();
+  if (Pos.row == 1) {
+    Coord move1 {2, Pos.col};
+    moves.push_back(move1);
+    Coord move2 {3, Pos.col};
+    moves.push_back(move2);
+  }
+  else if (Pos.row != 8) {
+    Coord move {Pos.row + 1, Pos.col};
+    moves.push_back(move);
+  }
+  
+  if (Pos.col == 1 && Pos.row != 8) {
+    Coord capture {Pos.row + 1, Pos.col + 1};
+    captures.push_back(capture);
+  }
+  if (Pos.col == 8 && Pos.row != 8) {
+    Coord capture {Pos.row + 1, Pos.col -1};
+    captures.push_back(capture);
+  }
+  elseif (Pos.row !=8 ) {
+    Coord capture1 {Pos.row + 1, Pos.col + 1};
+    captures.push_back(capture1);
+    Coord captures {Pos.row + 1, Pos,col - 1};
+    captures.push_back(capture2);
+  }
+  
 }
 
 Pawn::possibleMove(Coord Pos) {
-  
+  for (auto i : moves) {
+    if (i.col == Pos.col && i.row == Pos.row) return true;
+  }
+  return false;
 }
