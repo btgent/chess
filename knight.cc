761fd7887335d17ac0676c08b74abe99b1d756c2
btgent <btgent@hotmail.com>
2019-07-27 17:52:55 -0400

    Update knight.cc

--- knight.cc
+++ knight.cc
@@ -1,23 +1,26 @@
 #include "knight.h"
 #include <vector>
+using std::vector;
 
 Knight::Knight(Coord Pos, Colour colour): pos{Pos}, colour{colour} {
-  type = Knight;
-  if (Pos.col == 1) {
-    if (Pos.row == 1) {
-      Coord move1 {2, 3};
-      moves.push_back(move1);
-      Coord move2 {3, 2};
-      moves.push_back(move2);
-    }
-    else if (Pos.row == 8) {
-      Coord move1 {6, 2};
-      moves.push_back(move1);
-      Coord move2 {7, 3};
-      moves.push_back(move2);
-    }
-    else if (Pos.row == 2) {
-      
-    ]
+  type = Pawn;
+  firstMove = true;
+}
+
+bool Knight::possibleMove(Coord Pos) {
+  if ((Pos.col == pos.col + 2) || (Pos.col == pos.col - 2)) {
+    if ((Pos.row == pos.row + 1) || (Pos.row == pos - 1)) return true;
+    else return false;
   }
+  if ((Pos.col == pos.col + 1) || (Pos.col == pos.col - 1)) {
+    if ((Pos.row == pos.row + 2) || (Pos.row == pos - 2)) return true;
+    else return false;
+  }
+}
+
+vector<Coord> Knight::requiredEmpty(Coord Pos) {
+   if (possibleMove(Coord Pos)) {
+     vector<Coord> v;
+     v.push_back(Pos);
+   }
 }
