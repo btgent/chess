1a1552b91758c5fef24a2f33a6309d92cbfa3c17
btgent <btgent@hotmail.com>
2019-07-27 18:23:27 -0400

    Update knight.cc

--- knight.cc
+++ knight.cc
@@ -10,17 +10,19 @@ Knight::Knight(Coord Pos, Colour colour, Type type = Knight): pos{Pos}, colour{c
 bool Knight::possibleMove(Coord Pos) {
   if ((Pos.col == pos.col + 2) || (Pos.col == pos.col - 2)) {
     if ((Pos.row == pos.row + 1) || (Pos.row == pos - 1)) return true;
-    else return false;
   }
   if ((Pos.col == pos.col + 1) || (Pos.col == pos.col - 1)) {
     if ((Pos.row == pos.row + 2) || (Pos.row == pos - 2)) return true;
-    else return false;
   }
+  return false;
 }
 
 vector<Coord> Knight::requiredEmpty(Coord Pos) {
+  vector<Coord> v;
    if (possibleMove(Coord Pos)) {
-     vector<Coord> v;
      v.push_back(Pos);
+     return v;
    }
+  v.push_back(pos);
+  return v;
 }
