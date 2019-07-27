3686ed7ca7f11829f1ef7c28f30d3ba713163701
btgent <btgent@hotmail.com>
2019-07-27 16:23:28 -0400

    Update piece.cc

--- piece.cc
+++ piece.cc
@@ -1,10 +1,14 @@
 #include "piece.h"
 
-Piece::Piece(Coord pos, Type type, Colour colour): pos{pos}, type{type}, colour{colour} {}
+Piece::Piece(Coord pos, Type type, Colour colour): pos{pos}, type{type}, colour{colour}, firstMove(true) {}
 
 Piece::~Piece() {}
 
+ColourPiece::getColour() {
+  return colour;
+}
+
 Piece::setPos(Coord Pos) {
-  pos->row = Pos->row;
-  pos->col = Pos->col;
+  pos.row = Pos.row;
+  pos.col = Pos.col;
 }
