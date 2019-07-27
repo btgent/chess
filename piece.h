d784b81856b19b6a24bf515e7cef0ee594f50455
btgent <btgent@hotmail.com>
2019-07-27 16:18:36 -0400

    Update piece.h

--- piece.h
+++ piece.h
@@ -1,16 +1,20 @@
-#ifndef _PIECE_H_
-#define _PIECE_H_
+#ifndef __PIECE_H__
+#define __PIECE_H__
 #include "type.h"
 #include "move.h"
+#include <vector>
 
 class Piece {
   Coord pos;
   Type type;
   Colour colour;
+  bool firstMove;
  public:
   Piece(Coord pos, Type type, Colour colour);
-  ~Piece();
+  virtual ~Piece();
   Colour virtual getColour() = 0;
+  bool virtual possibleMove(Coord Pos) = 0;
+  std::vetor<Coord> requireEmpty(Coord Pos) = 0;
   bool virtual possibleMove(Coord Pos) = 0;
   void setPos(Coord Pos);
 };
