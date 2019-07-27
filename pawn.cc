600355928650f30f873784aa3b61d5cd9c75ea73
btgent <btgent@hotmail.com>
2019-07-27 17:29:09 -0400

    Update pawn.cc

--- pawn.cc
+++ pawn.cc
@@ -1,43 +1,62 @@
 #include "pawn.h"
 #include <vector>
+using std::vector;
 
 Pawn::Pawn(Coord Pos, Colour colour): pos{Pos}, colour{colour} {
   type = Pawn;
-  if (Pos.row == 1) {
-    Coord move1 {2, Pos.col};
-    moves.push_back(move1);
-    Coord move2 {3, Pos.col};
-    moves.push_back(move2);
-  }
-  else if (Pos.row != 8) {
-    Coord move {Pos.row + 1, Pos.col};
-    moves.push_back(move);
-  }
-  
-  if (Pos.col == 1 && Pos.row != 8) {
-    Coord capture {Pos.row + 1, Pos.col + 1};
-    captures.push_back(capture);
-  }
-  if (Pos.col == 8 && Pos.row != 8) {
-    Coord capture {Pos.row + 1, Pos.col -1};
-    captures.push_back(capture);
-  }
-  else if (Pos.row !=8 ) {
-    Coord capture1 {Pos.row + 1, Pos.col + 1};
-    captures.push_back(capture1);
-    Coord captures {Pos.row + 1, Pos,col - 1};
-    captures.push_back(capture2);
-  }
-  
-}
-
-Colour Pawn::getColour() {
-  return Colour; 
+  firstMove = true;
 }
 
 bool Pawn::possibleMove(Coord Pos) {
-  for (auto i : moves) {
-    if (i.col == Pos.col && i.row == Pos.row) return true;
+  switch(colour) {
+    case "White" :
+      if (pos.col == Pos.col) {
+        if (firstMove && Pos.row == (pos.row + 2) return true;
+        else if (Pos.row == (pos.row + 1) return true; 
+      }
+      else if (pos.col == (Pos.col + 1) && Pos.row == (pos.row + 1)) return true;
+      else if (pos.col == (Pos.col - 1) && Pos.row == (pos.row + 1)) return true;
+      else return false;
+    
+    case "Black" :
+      if (pos.col == Pos.col) {
+        if (firstMove && Pos.row == (pos.row - 2) return true;
+        else if (Pos.row == (pos.row - 1) return true; 
+      }
+      else if (pos.col == (Pos.col + 1) && Pos.row == (pos.row - 1)) return true;
+      else if (pos.col == (Pos.col - 1) && Pos.row == (pos.row - 1)) return true;
+      else return false;
   }
-  return false;
+}
+
+vector<Coord> Pawn::requiredEmpty(Coord Pos) {
+  if (possibleMove(Pos)) {
+    vector<Coord> v;
+    switch(colour) {
+      case "White" :
+        if (pos.col == Pos.col) {
+          for (int i = pos.row; i <= Pos.row; i++) {
+            Coord move{i, Pos.col};
+            v.push_back(move);
+          }
+        }
+      
+      case "Black" :
+        if (pos.col == Pos.col) {
+          for (int i = pos.row; i >= Pos.row; i--) {
+            Coord move{i, Pos.col};
+            v.push_back(move);
+          }
+        }
+    }
+  return v;
+  }
+}
+                 
+vector<Coord> Pawn::requiredOccupied(Coord Pos) {
+  if (possibleMove(Pos)) {
+    vector<Coord> v;
+    v.push_back(Pos);
+    return v;
+  }
 }
