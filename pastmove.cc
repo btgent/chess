e8ce61189b99cfb53be0ad3f5c65bbf23f6fb4f6
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 17:46:01 -0400

    Update pastmove.cc

--- pastmove.cc
+++ pastmove.cc
@@ -1,5 +1,5 @@
 #include "pastmove.h"
 #include "piece.h"
 
-PastMove::PastMove(Coord source, Coord dest, Type type, bool check=false)
+PastMove::PastMove(Coord source, Coord dest, Type type, bool check)
   : source{source}, dest{dest}, type{type}, check{check} {}
