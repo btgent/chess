c59b1be05f4a8df868c97b6ce14a2718d6c4c1e7
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 17:45:34 -0400

    Update futuremove.cc

--- futuremove.cc
+++ futuremove.cc
@@ -1,4 +1,4 @@
 #include "futuremove.h"
 
-FutureMove::FutureMove(Coord source, Coord dest, bool isPromo=false, Type promoType=Type::Queen)
+FutureMove::FutureMove(Coord source, Coord dest, bool isPromo, Type promoType)
   : source{source}, dest{dest}, isPromo{isPromo}, promoType{promoType} {}
