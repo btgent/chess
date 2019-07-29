12cf2fe6e0f85e4d4b37ab448b0974a8157508e7
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 16:37:19 -0400

    Update pastmove.h

--- pastmove.h
+++ pastmove.h
@@ -1,6 +1,12 @@
 #ifndef PASTMOVE_H
 #define PASTMOVE_H
 
+#include "coord.h"
+#include "type.h"
+#include <memory>
+class Piece;
+class PastMove;
+
 struct PastMove {
 	Coord source;
 	Coord dest;
@@ -8,7 +14,7 @@ struct PastMove {
 	bool check;  //If move puts the other colour in check
 	std::unique_ptr<Piece> capture; //Store the piece that was captured (if any)
 	std::unique_ptr<Piece> oldPromo; //Store the pawn when it gets promoted
-	std::unique_ptr<Move> additional; //For castling
+	std::unique_ptr<PastMove> additional; //For castling
 };
 
 #endif
