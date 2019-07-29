3e172ca4995adadf9a08d6351f12fe647359defc
btgent <btgent@hotmail.com>
2019-07-29 15:00:42 -0400

    Update graphicdisplay.cc

--- graphicdisplay.cc
+++ graphicdisplay.cc
@@ -1,7 +1,7 @@
 #include "GraphicDisplay.h"
 #include "xwindow.h"
 
-GraphicDisplay::GraphicDisplay() {
+GraphicDisplay::GraphicDisplay(int n) : boardSize(n) {
 }
 
 //left top corner start with (50,50) and each rectangle is 50*50
@@ -9,12 +9,12 @@ void GraphicDisplay::drawBoard(const Board & myb) {
 	Xwindow w;
 	int initx = 50;
 	int inity = 50;
-	int bsize = myb.getSize(); //should equal to 8
-	for (int i = 0; i < bsize; ++i) {  //i is height
+	//int bsize = myb.getSize(); //should equal to 8
+	for (int i = 0; i < n; ++i) {  //i is height
 		if (i % 2 == 0) {
-			for (int j = 0; j < bsize; ++j) { //white, Black
+			for (int j = 0; j < n; ++j) { //white, Black
 				if (j % 2 == 0) {
-					w.fillRectangle(initx + i*50, inity + j*50, 50, 50, xWindow::White);
+					w.fillRectangle(initx + i * 50, inity + j*50, 50, 50, xWindow::White);
 				}
 				else {
 					w.fillRectangle(initx + i * 50, inity + j * 50, 50, xWindow::Black);
@@ -22,11 +22,18 @@ void GraphicDisplay::drawBoard(const Board & myb) {
 			}
 		}
 		else {
-			for (int j = 0; j < bsize; ++j) {  //Black, White
-
+			for (int j = 0; j < n; ++j) {  //Black, White
+                        	if (j % 2 == 0) {
+					w.fillRectangle(initx + i * 50, inity + j * 50, 50, 50, xWindow::Black);
+				}
+				else {
+					w.fillRectangle(initx + i * 50, inity + j * 50, 50, xWindow::White);
+				}
 			}
 		}
 	}
+	
+	
 }
 
 GraphicDisplay::~GraphicDisplay() {
