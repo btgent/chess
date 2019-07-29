4df9f73f4e77591a1d7f3d1c7b3158b4b327fc0b
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 17:42:08 -0400

    Update piece.h

--- piece.h
+++ piece.h
@@ -1,23 +1,32 @@
 #ifndef __PIECE_H__
 #define __PIECE_H__
+#include "coord.h"
 #include "type.h"
-#include "move.h"
+#include "colour.h"
 #include <vector>
 
 class Piece {
   Coord pos;
-  Type type;
-  Colour colour;
- public:
+  const Type type;
+  const Colour colour;
   bool firstMove;
-  Piece(Coord pos, Colour colour, Type type);
+  
+ public:
+  Piece(Coord pos, Type type, Colour colour, bool firstMove=true);
   virtual ~Piece();
-  Colour getColour();
-  Type getType();
+  
   Coord getPos();
-  bool virtual possibleMove(Coord Pos) = 0;
-  std::vetor<Coord> virtual requireEmpty(Coord Pos) = 0;
-  void setPos(Coord Pos);
+  Type getType();
+  Colour getColour();
+  bool isFirstMove();
+  void setPos(Coord);
+
+  virtual bool possibleMove(Coord dest) = 0;
+  virtual bool possibleMove(int r, int c) = 0;
+  virtual std::vector<Coord> requiredEmpty(Coord dest) = 0;
+  virtual std::vector<Coord> requiredEmpty(int r, int c) = 0;
+  virtual std::vector<Coord> requiredOccupied(Coord dest) = 0;
+  virtual std::vector<Coord> requiredOccupied(int r, int c) =0;
 };
 
 #endif
