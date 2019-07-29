50c08783f5a76fd1a5a60f9f46c612c1f5d981b1
wenrens <32319907+wenrens@users.noreply.github.com>
2019-07-29 16:45:23 -0400

    Update testboard.cc

--- testboard.cc
+++ testboard.cc
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+enum Type {
+  King, Queen, Rook, Bishop, Knight, Pawn
+};
+
+enum Colour { Black, White };
+
+struct Piece {
+  Coord pos;
+  Type type;
+  Colour colour;
+};
+
+struct Coord {
+	int row, col;
+};
+
 int main {
 Xwindow w;
 	int initx = 50;
@@ -32,6 +48,34 @@ Xwindow w;
 		}
 	}
   
+
+Coord ck = {0, 0};
+coord cq = {2, 2};
+Piece pk = {ck, Type::King, Colour::White};
+Piece pq = {cq, Type::Queen, Colour::Black};
+Piece * parr[2] = {pk, pq};
+
+	for (int i = 0; i < 2; ++i) {
+		Piece thePiece = parr[i];
+		int px = thePiece.pos.row;
+		int py = thePiece.pos.col;
+		Colour::c = thePiece.colour();
+		switch (c) {
+		case Black: Xwindow::Pcol = Xwindow::Red;
+		case White: Xwindow::Pcol = Xwindow::Blue;
+		}
+
+		Type t = thePiece.type();
+		switch (t) {
+		case King: w.drawBigString(initx + px * size, inity + py * size, "K", Pcol);
+		case Queen: w.drawBigString(initx + px * size, inity + py * size, "Q", Pcol);
+		case Rook: w.drawBigString(initx + px * size, inity + py * size, "R", Pcol);
+		case Bishop: w.drawBigString(initx + px * size, inity + py * size, "B", Pcol);
+		case Knight: w.drawBigString(initx + px * size, inity + py * size, "N", Pcol);
+		case Pawn: w.drawBigString(initx + px * size, inity + py * size, "P", Pcol);
+		}
+	
+	
   
   char c;
   cin >> c;
