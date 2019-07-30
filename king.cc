806ef3420dd064c7986b54a8e8f6aa303ba69108
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 00:34:57 -0400

    Update king.cc

--- king.cc
+++ king.cc
@@ -2,24 +2,41 @@
 #include <vector>
 using std::vector;
 
-King::King(Coord Pos, Colour colour, Type type = King): pos{Pos}, colour{colour}, type{type} {
-  type = King;
-  firstMove = true;
-}
+King::King(Coord Pos, Colour colour, bool firstMove)
+  : Piece{pos, colour, Type::King, firstMove} {}
 
-bool King::possibleMove(Coord Pos) {
-  if ((Pos.row == pos.row) && (Pos.col == pos.col)) return false;
-  elif (((Pos.row - pos.row) >= -1) && ((Pos.row - pos.row) <= 1)) {
-    if (((Pos.col - pos.col) >= -1) && ((Pos.col - pos.col) <= 1)) return true;
-  }
+int abs(int i)  {return (i>0) ? i : -i;}
+
+bool King::possibleMove(Coord dest) const {
+  if ((dest.row == pos.row) && (dest.col == pos.col)) return false;
+  if ( (abs(pos.row - dest.row) <= 1) && (abs(pos.col - dest.col) <= 1) ) return true;
   return false;
 }
+bool King::possibleMove(int r, int c) const {
+  return possibleMove(Coord{.row=r, .col=c});
+}
 
-vector<Coord> King::requiredEmpty(Coord Pos) {
+vector<Coord> King::requiredEmpty(Coord dest) const {
   vector<Coord> v;
-  if (possibleMove(Pos)) {
-    v.push_back(Pos);
+  // as long as the move is possible, king doesn't need to traverse any squares
+  if (!possibleMove(dest)) {
+    v.push_back(pos);
   }
-  v.push_back(pos);
-  return v;  
+  return v;
+}
+vector<Coord> King::requiredEmpty(int r, int c) const {
+  return requiredEmpty(Coord{.row=r, .col=c});
+}
+
+vector<Coord> King::requiredOccupied(Coord dest) const {
+  vector<Coord> v;
+  // all standard pieces except pawn don't require an enemy piece to move
+  // in a certain way
+  if  (!possibleMove(dest)) {
+    v.push_back(pos);
+  }
+  return v;
+}
+vector<Coord> King::requiredOccupied(int r, int c) const {
+  return requiredOccupied(Coord{.row=r, .col=c});
 }
