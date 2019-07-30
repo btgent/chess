e3cec7783eed91d7fcf36c89431be855544cf562
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 19:21:38 -0400

    Update queen.cc

--- queen.cc
+++ queen.cc
@@ -4,7 +4,7 @@ using std::vector;
 
 Queen::Queen(Coord pos, Colour colour, bool firstMove)
   : Piece{pos, colour, Type::Queen, firstMove} {}
-Piece *Queen::clone() { return new Queen(*this); }
+Piece *Queen::clone() const { return new Queen(*this); }
 
 int abs(int i)  {return (i>0) ? i : -i;}
 
