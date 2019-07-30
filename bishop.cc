c2dfc7fc234be5ec89b35e467bb404e1c3011957
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 22:08:39 -0400

    Update bishop.cc

--- bishop.cc
+++ bishop.cc
@@ -2,52 +2,71 @@
 #include <vector>
 using std::vector;
 
-Bishop::Bishop(Coord Pos, Colour colour, Type type = Bishop): pos{Pos}, colour{colour}, type{type} {
-  type = Bishop;
-  firstMove = true;
-}
+Bishop::Bishop(Coord Pos, Colour colour, bool firstMove)
+  : Piece{pos, colour, Type::Bishop, firstMove} {}
 
-bool Bishop::possibleMove(Coord Pos) {
-  if ((Pos.row == pos.row) && (Pos.col == pos.col)) return false;
-  else if ((Pos.row - pos.row) == (Pos.col - pos.col)) return true;
-  else if ((Pos.row - pos.row) == (pos.col - Pos.col)) return true;
+int abs(int i)  {return (i>0) ? i : -i;}
+
+bool Bishop::possibleMove(Coord dest) const {
+  if ((dest.row == pos.row) && (dest.col == pos.col)) return false;
+  else if (abs(dest.row - pos.row) == abs(dest.col - pos.col)) return true;
+  else if (abs(dest.row - pos.row) == abs(pos.col - dest.col)) return true;
   return false;
 }
+bool Bishop::possibleMove(int r, int c) const {
+  return possibleMove(Coord{.row=r, .col=c});
+}
 
-vector<Coord> Bishop::requiredEmpty(Coord Pos) {
+vector<Coord> Bishop::requiredEmpty(Coord dest) const {
   vector<Coord> v;
-  if (possibleMove(Pos)) {
-    if (Pos.row > pos.row) {
-      if (Pos.col > pos.col) {
-        for (int i = 1; (pos.row + i) <= Pos.row; i++) {
-          Coord move {pos.row + i, pos.col + i};
-          v.push_back(move);
-        }
-      }
-      else {
-        for (int i = 1; (pos.row + i) <= Pos.row; i++) {
-          Coord move {pos.row + i, pos.col - i};
-          v.push_back(move);
-        }
-      }
-    }
-    
-    else {
-      if (Pos.col > pos.col) {
-        for (int i = 1; (pos.row - i) >= Pos.row; i++) {
-          Coord move {pos.row - i, pos.col + i};
-          v.push_back(move);
-        }
-      }
-      else {
-        for (int i = 1; (pos.row - i) >= Pos.row; i++) {
-          Coord move {pos.row - i, pos.col - i};
-          v.push_back(move);
-        }
-      }
-    }
+  if (!possibleMove(dest)) {
+    v.push_back(pos);
     return v;
   }
-  v.push_back(pos);
+  if (dest.row > pos.row) {
+    if (dest.col > pos.col) {
+      // NE
+      for (int i = 1; (pos.row + i) < dest.row; i++) {
+        Coord move {pos.row + i, pos.col + i};
+        v.push_back(move);
+      }
+    } else {
+      // NW
+      for (int i = 1; (pos.row + i) < dest.row; i++) {
+        Coord move {pos.row + i, pos.col - i};
+        v.push_back(move);
+      }
+    }
+  } else {
+    if (dest.col > pos.col) {
+      // SE
+      for (int i = 1; (pos.row - i) > dest.row; i++) {
+        Coord move {pos.row - i, pos.col + i};
+        v.push_back(move);
+      }
+    } else {
+      // SW
+      for (int i = 1; (pos.row - i) > dest.row; i++) {
+        Coord move {pos.row - i, pos.col - i};
+        v.push_back(move);
+      }
+    }
+  }
   return v;
+}
+vector<Coord> Bishop::requiredEmpty(int r, int c) const {
+  return requiredEmpty(Coord{.row=r, .col=c});
+}
+
+vector<Coord> Bishop::requiredOccupied(Coord dest) const {
+  vector<Coord> v;
+  // all standard pieces except pawn don't require an enemy piece to move
+  // in a certain way
+  if  (!possibleMove(dest)) {
+    v.push_back(pos);
+  }
+  return v;
+}
+vector<Coord> Bishop::requiredOccupied(int r, int c) const {
+  return requiredOccupied(Coord{.row=r, .col=c});
 }
