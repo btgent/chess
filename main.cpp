e2e593e936e2d717a4f8d3c6a10162a117298dd4
wenrens <32319907+wenrens@users.noreply.github.com>
2019-07-30 15:39:43 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <score.h>
+#include <board.h>
+
+using namespace std;
+
+int main() {
+	string s;
+	while (cin >> s) {
+		if (s == "game") {
+			Score *scoreb = new Score(); //create a scoreBoard with white:0, Black:0
+			string p1, p2;
+			if (cin >> p1) { //player1 setup
+				if (p1 == "human") {
+					Human h1(Colour::White);
+				}
+				else if (p1 == "computer1") {
+					Computer1(Colour::White);
+				}
+				else if (p1 == "computer2") {
+					Computer2(Colour::White);
+				}
+				else if (p1 == "computer3") {
+					Computer3(Colour::White);
+				}
+				else { //invalid input for player
+
+				}
+			}
+			if (cin >> p2) {   //player2 setup
+				if (p2 == "human") {
+					Human h2(Colour::Black);
+				}
+				else if (p2 == "computer1") {
+					Computer1(Colour::Black);
+				}
+				else if (p2 == "computer2") {
+					Computer2(Colour::Black);
+				}
+				else if (p2 == "computer3") {
+					Computer3(Colour::Black);
+				}
+				else { //invalid input for player
+
+				}
+			}
+		}
+		else if (s == "resign") {
+			scoreb->drawAdd();
+			//clear the board
+		}
+		else if (s == "move") {
+			
+			myb->move();
+		}
+		else if (s == "setup") {
+			Board * myb; //create an empty board
+			string op;
+			while (cin >> op) {
+				if (op == "+") {  //place piece case
+
+				}
+				else if (op == "-") { //remove piece case
+
+				}
+				else if (op == "=") { //turn colour case
+
+				}
+				else if (op == "done") {
+					break;
+				}
+			}
+			//check king and pawn
+		}
+	}
+	scoreb->printScore();
+	delete scoreb;
+}
