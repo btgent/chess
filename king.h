f1072b6fa13e916210e370b34c0cd460989170bc
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-29 21:29:52 -0400

    Update king.h

--- king.h
+++ king.h
@@ -2,13 +2,17 @@
 #define __KING_H__
 #include "piece.h"
 #include <vector>
-using std::vector;
 
 class King: public Piece {
  public:
-  King(Coord pos, Colour colour, Type type = King);
-  bool possibleMove(Coord Pos);
-  vector<Coord> requiredEmpty(Coord Pos);
+  King(Coord pos, Colour colour, bool firstMove=true);
+  
+  bool possibleMove(Coord dest) const override;
+  bool possibleMove(int r, int c) const override;
+  std::vector<Coord> requiredEmpty(Coord dest) const override;
+  std::vector<Coord> requiredEmpty(int r, int c) const override;
+  std::vector<Coord> requiredOccupied(Coord dest) const override;
+  std::vector<Coord> requiredOccupied(int r, int c) const override;
 };
 
 #endif
