1002bf02d6d821766aeab5709487170d8ceb41f1
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 18:38:54 -0400

    Update setupexception.h

--- setupexception.h
+++ setupexception.h
@@ -3,6 +3,8 @@
 
 #include <stdexcept>
 
+// Thrown when a setup function is called in non-setup mode or
+// when a non-setup function is called in setup mode (referring to class Board)
 class SetupException : public std::runtime_error {
 public:
   SetupException(const char* m) : std::runtime_error(m) { }
