0b27d236299b9224a55b8a7d0e24717c9d1bd168
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 19:28:10 -0400

    Update moveexception.h

--- moveexception.h
+++ moveexception.h
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 // Thrown when a move causes the king with the same colour to be in check
+// or there is no move on stackMove to undo
 class MoveException : public std::runtime_error {
 public:
   MoveException(const char* m) : std::runtime_error(m) { }
