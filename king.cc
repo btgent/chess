19ccb5fc44b3ae744331c3be5dabe9ffcceb0a47
wenrens <32319907+wenrens@users.noreply.github.com>
2019-07-31 01:51:09 -0400

    Update king.cc

--- king.cc
+++ king.cc
@@ -6,11 +6,11 @@ King::King(Coord pos, Colour colour, bool firstMove)
   : Piece{pos, colour, Type::King, firstMove} {}
 Piece *King::clone() const { return new King(*this); }
 
-int abs(int i)  {return (i>0) ? i : -i;}
+int intabs(int i)  {return (i>0) ? i : -i;}
 
 bool King::possibleMove(Coord dest) const {
   if ((dest.row == pos.row) && (dest.col == pos.col)) return false;
-  if ( (abs(pos.row - dest.row) <= 1) && (abs(pos.col - dest.col) <= 1) ) return true;
+  if ( (intabs(pos.row - dest.row) <= 1) && (intabs(pos.col - dest.col) <= 1) ) return true;
   return false;
 }
 bool King::possibleMove(int r, int c) const {
