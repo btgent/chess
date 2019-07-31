91d5ab77470573d7d3ff9b2ab59ba9198d30d595
wenrens <32319907+wenrens@users.noreply.github.com>
2019-07-31 01:05:31 -0400

    Update score.h

--- score.h
+++ score.h
@@ -1,9 +1,12 @@
-#pragma once
+
 class Score {
 	float whiteScore;
 	float blackScore;
 public:
 	Score(int ws=0, int bs=0);
+	void drawAdd();
+	void whiteAdd();
+	void blackAdd();
 	~Score();
 	void printScore();
 };
