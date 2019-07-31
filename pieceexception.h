7e015a7a04472d308c742547a4d5e7f47a365605
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 20:21:48 -0400

    Update pieceexception.h

--- pieceexception.h
+++ pieceexception.h
@@ -3,8 +3,9 @@
 
 #include <stdexcept>
 
-// Thrown when a piece doesn't exist at the location specified or
-// is the wrong colour or cannot be placed there
+// Thrown when a piece doesn't exist at the location specified,
+// is the wrong colour, cannot be placed there or piece doesn't exist
+// at that number in listPieces
 class PieceException : public std::runtime_error {
 public:
   PieceException(const char* m) : std::runtime_error(m) { }
