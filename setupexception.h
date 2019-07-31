bf01161fc6c029761730bc6f5791cc540e17ee4f
SaltOverflow <37456194+SaltOverflow@users.noreply.github.com>
2019-07-30 20:02:32 -0400

    Update setupexception.h

--- setupexception.h
+++ setupexception.h
@@ -3,8 +3,10 @@
 
 #include <stdexcept>
 
-// Thrown when a setup function is called in non-setup mode or
-// when a non-setup function is called in setup mode (referring to class Board)
+// Thrown when a setup function is called in non-setup mode,
+// when a non-setup function is called in setup mode,
+// setupMode is false in Board constructor while size is not 8 or
+// cannot exit setup mode because invariants have not been satisfied yet
 class SetupException : public std::runtime_error {
 public:
   SetupException(const char* m) : std::runtime_error(m) { }
