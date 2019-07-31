53e5031c6efe06faceb7e3370b54c3953f69b339
wenrens <32319907+wenrens@users.noreply.github.com>
2019-07-31 01:04:42 -0400

    Update score.cc

--- score.cc
+++ score.cc
@@ -6,6 +6,19 @@ Score::Score(int ws, int bs) :whiteScore{ ws }, blackScore{ bs } {}
 
 Score::~Score() {}
 
+void Score::drawAdd() {
+	whiteScore += 0.5;
+	blackScore += 0.5;
+}
+
+void Score::whiteAdd() {
+	whiteScore += 1;
+}
+
+void Score::blackAdd() {
+	blackScore += 1;
+}
+
 void Score::printScore() {
 	std::cout << "Final Score:" << std::endl;
 	std::cout << "White: " << whiteScore << std::endl;
